0c95327c9498cb12bc1ec1e5ca2cfa9d64cda6ac
Anouk <a.prins@student.ru.nl>
2018-10-31 13:28:56 +0100

    testje

---
+++ src/adsprac1/ADSPrac1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adsprac1;
+
+/**
+ *
+ * @author Anouk
+ */
+public class ADSPrac1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // TEST
+    }
+    
+}
